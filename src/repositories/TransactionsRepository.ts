cc62ab63a9af42891051531872fd2955c414c96c
Guilherme Barbosa <gbarbosabuenodasilva@gmail.com>
2020-06-23 15:15:26 -0300

    Desafio finalizado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -5,6 +5,11 @@ interface Balance {
   outcome: number;
   total: number;
 }
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -14,15 +19,31 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((acumulador, item) => {
+      if (item.type === 'income') {
+        return acumulador + item.value;
+      }
+      return acumulador;
+    }, 0);
+    const outcome = this.transactions.reduce((acumulador, item) => {
+      if (item.type === 'outcome') {
+        return acumulador + item.value;
+      }
+      return acumulador;
+    }, 0);
+
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
